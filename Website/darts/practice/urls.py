7650b3a481f94fd75e678b2277b7ce458e2a421c
angus-galbraith <angus.galbraith25@outlook.com>
2023-07-20 21:30:21 +0100

    django version working

--- Website/darts/practice/urls.py
+++ Website/darts/practice/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="darts"),
+    path("rtbstart", views.rtbstart, name="rtb"),
     path("rtb/", views.rtb, name="rtb"),
+    path("rtbover", views.rtbover, name="rtbover"),
     path("finishes/", views.finishes, name="finishes"),
+    path("highscores", views.highscores, name="highscores")
 
 
 
--- Website/darts/practice/views.py
+++ Website/darts/practice/views.py
@@ -1,14 +1,77 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import rtbHighScore
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
+
+def rtbstart(request):
+    if request.method == 'POST':
+        playername = request.POST["playername"]
+        request.session["playername"] = playername
+        message = True
+        context = {"playername" : playername, "message" : message}
+        return render(request, 'rtbstart.html', context)
+    else:
+        return render(request, 'rtbstart.html')
+
+
+
+
+global toGoFor
+global runningTotal
+
+
+toGoFor = 1
+runningTotal = 0
+
 def rtb(request):
-    toGoFor = 1 
-    dict = { "toGoFor" : toGoFor}
-    return render(request, 'rtb.html', dict)
+    playername = request.session["playername"]
+    
+    
+    if request.method == 'POST':
+        
+        numbHit = int(request.POST["numberHit"])
+        global toGoFor
+        global runningTotal
+        score = numbHit * toGoFor
+        runningTotal += score
+        toGoFor += 1
+        if toGoFor == 21:
+            bulls = "Bulls"
+            dict = { "toGoFor" : bulls, "runningTotal" : runningTotal, "playername" : playername }
+        else:
+            dict = { "toGoFor" : toGoFor, "runningTotal" : runningTotal, "playername" : playername }
+        if toGoFor == 5:
+            gameover = True
+            dict = {"runningTotal" : runningTotal, "gameover" : gameover}
+            newEntry = rtbHighScore(name = playername, score = runningTotal)
+            newEntry.save()
+            return render(request, "rtb.html", dict)
+        else:
+            dict = { "toGoFor" : toGoFor, "runningTotal" : runningTotal, "playername" : playername }
+            return render(request, 'rtb.html', dict)
+    else: 
+        
+        toGoFor = 1
+        runningTotal = 0
+        
+        dict = { "toGoFor" : toGoFor, "runningTotal" : runningTotal, "playername" : playername }
+        return render(request, 'rtb.html', dict)
+    
+def rtbover(request):
+    
+    dict = {"runningTotal" : runningTotal}
+    return (request, "rtbover.html", dict)
+
 
 def finishes(request):
     return render(request, 'finishes.html')
+
+def highscores(request):
+    return render(request, 'highscores.html', {"scores" : rtbHighScore.objects.all().order_by('-score').values()})
+
+
+
